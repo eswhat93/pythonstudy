380d3d39335f7d139a91d054293ccb4e91719ede
eswhat93 <adleeeunsol@gmail.com>
2020-05-14 13:39:55 +0900

    master

--- 20200511.py
+++ 20200511.py
@@ -19,8 +19,61 @@ print(bubble_sort(a))
 
 #삽입정렬
 #20200512 이지롱
-a=[2,1]
+a=[2,1,5,3,6]
+#배우기 전 코드
 def insert_sort(data):
     swap=0
-    for i in len(data)-1:
-        for j in len(data)-1:
+    for i in range(len(data)-1):
+        for j in range(len(data)-i-1):
+            if data[j]>data[i+1]:
+                data[i+1],data[j] = data[j],data[i+1]
+                swap +=1
+        if swap ==0: break
+    return data
+
+#print(insert_sort(a))
+
+#https://visualgo.net/en/sorting
+
+#삽입정렬은 두번째 인덱스부터 시작
+#해당인덱스 (key값) 앞에 있는 데이터(B)부터 비교해서 key값이 더 작으면 B를 key 뒤로 보냄(B값을 뒤 인덱스로 복사)
+#이를 key값이 더 큰 데이터를 만날때까지 반복, 그리고 큰 데이터를 만난 위치 바로 뒤에 key값을 이동
+###range(시작, 끝, 스텝)
+
+#배우고나서 코드
+def insert_sort2(data):
+    for index in range(len(data)):
+        key = data[index]
+        for index2 in range(index,0,-1):
+            if data[index2-1]>key :
+                data[index2-1],data[index] = data[index],data[index2-1] 
+            else :
+                break
+    return data
+print(insert_sort2(a))
+
+
+# 큐 (QUEUE)
+# 가장 먼저 넣은 데이터를 가장 먼저 꺼낸다 FIFO = first in first out
+import queue
+
+data_queue = queue.Queue()
+
+data_queue.put("funcoding")
+data_queue.put("1")
+
+print(data_queue.qsize())
+
+data_queue.get()
+
+#Last in first out
+data_queue2 = queue.LifoQueue()
+
+#key, value값 넣고 뽑기
+data_queue3 = queue.PriorityQueue()
+
+data_queue3.put((10, "korea"))
+data_queue3.put((5, 1))
+data_queue3.put((15, "china"))
+
+#큐가 어디 많이쓰이나? 멀티태스킹을 위한 프로세스 스케쥴링 방식 구현 위해 (운영체제)
