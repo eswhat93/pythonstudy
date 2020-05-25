18d57f9277b617b55cd83252db3b846657643de5
eswhat93 <adleeeunsol@gmail.com>
2020-05-25 17:58:14 +0900

    master

--- 20200521.py
+++ 20200521.py
@@ -19,4 +19,30 @@ def solution(array, commands):
 array1 = [1, 5, 2, 6, 3, 7, 4]
 commands1 = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 
-print(solution(array1, commands1))
+#print(solution(array1, commands1))
+
+
+#20200525
+#1와 0로 채워진 표(board)가 있습니다. 표 1칸은 1 x 1 의 정사각형으로 이루어져 있습니다. 표에서 1로 이루어진 가장 큰 정사각형을 찾아 넓이를 return 하는 solution 함수를 완성해 주세요. 
+def solution(board):
+    answer = 1234
+    cnt=1
+    stand=0
+
+    # 첫번째 줄에 1이 있으면 연속되는 길이를 구한다
+    for i in range(len(board)-1):
+        if board[i] == 1:
+            stand = 1
+        else:
+            stand = 0
+        for j in range(i+1,len(board)):
+            if board[j] == stand :
+                cnt += 1
+            else:
+                cnt = 1
+    return cnt
+
+data = [1,0,0,1]
+#data = [[0,1,1,1],[1,1,1,1],[1,1,1,1],[1,0,1,0]]
+
+print(solution(data))
